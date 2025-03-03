2de6649b793431288cf6ec4a74c54ae824e5a949
NMayerOnyx <nicolasmayer19@gmail.com>
2025-03-03 11:07:38 +0100

    Commit initial

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+import random
+
+class Question :
+
+    """Classe pour question"""
+
+    def __init__ (self) :
+        self.enonce = None
+        self.repa = None
+        self.repb = None
+        self.repc = None
+        self.repd = None
+        self.bonnerep = None
+
+
+
